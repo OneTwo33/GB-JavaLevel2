992aad4d958f792cb78c967cfbac3f9406daa7d1
OneTwo33 <alex_tsai@mail.ru>
2021-02-11 01:38:58 +0300

    Выполненная пятая домашка

---
+++ src/ru/company/onetwo33/homework5/ThreadFill.java
@@ -0,0 +1,17 @@
+package ru.company.onetwo33.homework5;
+
+class ThreadFill implements Runnable {
+    private float[] arr;
+
+    public ThreadFill(float[] arr) {
+        this.arr = arr;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+
+}
